3d83f7eeca241b4cb0211341618f7c49ad3c7749
Anthony <dstdaskjl@gmail.com>
2022-03-15 17:00:43 -0400

    Fixed dimension error Fixed loss function name error

--- cnn.py
+++ cnn.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
@@ -6,11 +7,15 @@ class CNN:
     def asl_text(self, x: np.array, y: np.array):
         # normalizing
         x = x / 255.0
+        image_count = len(x)
+        pixel_count = len(x[0])
+        channel_count = 1
+        x = x.reshape(image_count, pixel_count, pixel_count, channel_count)
 
         model = Sequential()
 
         # first layer
-        model.add(Conv2D(64, (3, 3), input_shape=(*x.shape, 3)))
+        model.add(Conv2D(64, (3, 3), input_shape=(pixel_count, pixel_count, channel_count)))
         model.add(Activation("relu"))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -27,6 +32,6 @@ class CNN:
         model.add(Dense(1))
         model.add(Activation("sigmoid"))
 
-        model.compile(loss="categorical", optimizer='adam', metrics=['accuracy'])
+        model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
 
         model.fit(x, y, batch_size=1, epochs=2, validation_split=.2)
