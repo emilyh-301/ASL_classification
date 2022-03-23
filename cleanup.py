0ac3a904cfc377184dfb2e42b59166064072b724
Anthony <dstdaskjl@gmail.com>
2022-03-22 20:45:55 -0400

    Changed main name to research Added function to kill all processes

--- cleanup.py
+++ cleanup.py
@@ -1,10 +1,22 @@
+import sys
+import time
 from path import Path
+from filelock import FileLock
 
 p = Path()
+
+if len(sys.argv) == 2 and sys.argv[1] == 'kill':
+    with FileLock('status'):
+        p.write('status.txt', '1')
+    time.sleep(3)
 
 p.delete('Models')
 p.delete('Exceptions')
 p.delete('used.txt')
+p.delete('status.txt')
+p.delete('research.py')
 p.create('Models')
 p.create('Exceptions')
 p.create('used.txt')
+p.write('status.txt', '1')
+p.copy('main.py', 'research.py')
