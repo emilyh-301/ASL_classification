28e64a6159d9fb76b95aa9fc30433f3c7c4c9092
Anthony <dstdaskjl@gmail.com>
2022-03-24 14:37:36 -0400

    Increased efficiency

--- trace.py
+++ trace.py
@@ -4,17 +4,20 @@ import threading
 from filelock import FileLock
 from path import Path
 
-p = Path()
+path = Path()
 
 
 class Trace:
     def status(self) -> None:
         pid = os.getpid()
         while True:
-            with FileLock('status'):
-                status = p.read(filepath='status.txt')[0]
-                if status[:2] == '11':
-                    os.system('kill ' + str(pid))
+            kill = False
+            with FileLock(path.var.status_lock):
+                status = path.read(filepath=path.var.status_filepath)[0]
+                if status[:2] == path.var.dead:
+                    kill = True
+            if kill:
+                os.system('kill ' + str(pid))
             time.sleep(0.5)
 
     def start(self) -> None:
