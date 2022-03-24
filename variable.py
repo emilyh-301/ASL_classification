febe25d729c159db1f07ff7f67215d8ce6e29025
Anthony <dstdaskjl@gmail.com>
2022-03-24 14:37:43 -0400

    Initial commit

---
+++ variable.py
@@ -0,0 +1,9 @@
+class Variable:
+    read_lock = 'Shared/read'
+    status_lock = 'Shared/status'
+    used_lock = 'Shared/used'
+    status_filepath = 'Shared/status.txt'
+    used_filepath = 'Shared/used.txt'
+    model_dir = 'Models/'
+    alive = '1'
+    dead = '11'
