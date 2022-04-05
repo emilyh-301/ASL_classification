a98e0df1b6ebb505d62db95359be3005d8ec0e68
Anthony <dstdaskjl@gmail.com>
2022-04-05 00:57:19 -0400

    Added history directory

--- cleanup.py
+++ cleanup.py
@@ -5,10 +5,16 @@ path = Path()
 path.delete('Models')
 path.delete('Exceptions')
 path.delete('Shared')
+path.delete('History')
 path.delete('research.py')
+
 path.create('Models')
 path.create('Exceptions')
 path.create('Shared')
+path.create('History')
+
 path.create(path.var.used_filepath)
+path.create(path.var.alive_filepath)
 path.write(path.var.status_filepath, '1')
+
 path.copy('main.py', 'research.py')
