b9e0f0894a50168b9b9afb5696a608490417afa5
Anthony <dstdaskjl@gmail.com>
2022-04-15 00:14:52 -0400

    Moved condition check

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Emily Haigh and Anthony An
 # American Sign Language to text
 # Starting with A-Z
+import sys
+if len(sys.argv) != 2:
+    print('\nSet the command number:\n\t1: Training\n\t2: Test\n\t9: Data Preprocessing\n\n')
+    sys.exit(1)
+cmd = int(sys.argv[1])
+
 
 # Disable TF warnings
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-import sys
 
 # https://pythonprogramming.net/loading-custom-data-deep-learning-python-tensorflow-keras/
 from cnn import CNN
@@ -15,11 +20,6 @@ from trace import Trace
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('\nSet the command number:\n\t1: Training\n\t2: Test\n\t9: Data Preprocessing\n\n')
-        sys.exit(1)
-    cmd = int(sys.argv[1])
-
     # Training
     if cmd == 1:
         Trace().start()
