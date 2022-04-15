2fae53d2c7d3e1373f1595b592f32e8042cffa56
dstdaskjl <55066249+dstdaskjl@users.noreply.github.com>
2022-04-15 02:03:59 -0400

    Updated cleanup targets

--- cleanup.py
+++ cleanup.py
@@ -2,16 +2,12 @@ from path import Path
 
 path = Path()
 
-path.delete('Models')
-path.delete('Exceptions')
+path.delete('Exception')
 path.delete('Shared')
-path.delete('History')
 path.delete('research.py')
 
-path.create('Models')
-path.create('Exceptions')
+path.create('Exception')
 path.create('Shared')
-path.create('History')
 
 path.create(path.var.used_filepath)
 path.create(path.var.alive_filepath)
